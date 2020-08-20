93b02f77d64fe9a2938d776452125fa9e00583be
Jedidiah May <jedmay83@gmail.com>
2020-08-20 13:32:27 -0500

    Redirect to edit/add new page

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -25,10 +25,18 @@ namespace OdeToFood.Pages.Restaurants
             this.restaurantData = restaurantData;
             this.htmlHelper = htmlHelper;
         }
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            Restaurant = restaurantData.GetById(restaurantId);
+            if(restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+
+            }
             if(Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
@@ -42,6 +50,7 @@ namespace OdeToFood.Pages.Restaurants
             {
                 Restaurant = restaurantData.Update(Restaurant);
                 restaurantData.Commit();
+                return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
             }
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             return Page();
