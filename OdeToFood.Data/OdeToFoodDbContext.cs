5e7b59ec36875d8209536a732c94463a72829702
Jedidiah May <jedmay83@gmail.com>
2020-08-21 12:14:28 -0500

    localdb SQL setup with connections strings and options, no migrations yet

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -6,8 +6,13 @@ using System.Text;
 
 namespace OdeToFood.Data
 {
-    public class OdToFoodDbContext : DbContext
+    public class OdeToFoodDbContext : DbContext
     {
+
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+
+        }
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +20,12 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+
             services.AddRazorPages();
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
         }
