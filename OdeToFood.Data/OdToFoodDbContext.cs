a86b2b60a359c8684e0dbe851b70a30fb97e39f7
Jedidiah May <jedmay83@gmail.com>
2020-08-21 09:37:31 -0500

    start of EF Core

---
+++ OdeToFood.Data/OdToFoodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public class OdToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
